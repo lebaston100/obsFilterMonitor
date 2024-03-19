24e2a8b5943510c3a35e717a84ecb1ae81629ce3
theCYBRIX <math_jon207@hotmail.com>
2024-03-18 20:28:05 -0500

    fix: fixed obs crashing on script reload or close issue: obs must be closed in order to kill the server.

--- monitorconfig.py
+++ monitorconfig.py
@@ -5,21 +5,70 @@ import os.path
 from http.server import SimpleHTTPRequestHandler
 from socketserver import TCPServer
 from threading import Thread
+import time
 
 # TODO : Implements saving and loading settings to a json file (backup settings)
 
 class GlobalData:
     def __init__(self) -> None:
-        self.httpService : TCPServer = None
-        self.obsProperties = None
-        self.obsData = None
-        self.debug : bool = False
+        self.http_service : TCPServer = None
+        self.server_running : bool = False
+        self.obs_properties = None
+        self.obs_data = None
+        self.debug : bool = True
+        self.shutdown_requested : bool = False
+
+
+    def serve_settings(self):
+        try:
+            self.server_running = True
+            with TCPServer(("", PORT), SettingsRequestHandler) as service:
+                self.http_service = service
+                self.debug_message(f"Serving on port {PORT}")
+                service.serve_forever(0.5)
+                self.debug_message('Server shutdown.')
+        finally:
+            self.server_running = False
+            if self.http_service:
+                self.http_service.server_close()
+                self.http_service = None
+
+
+    def start_server_asynch(self, retry_freq : float = 0.5, tiemout : float = 10.0):
+        server_thread : Thread = Thread(name="HTTP Server", target=lambda: self.start_server(retry_freq, tiemout), daemon=False)
+        server_thread.start()
+
     
+    def start_server(self, retry_freq : float = 0.5, tiemout : float = 10.0):
+        self.shutdown_requested = False
+        start_time : float = time.process_time()
+        while tiemout == None or (time.process_time() - start_time) < tiemout:
+            if self.shutdown_requested: break
+            try:
+                self.debug_message('Starting settings server...')
+                self.serve_settings()
+            except BaseException as error:
+                self.debug_message(f'Unable to start server: {error}')
+                if self.shutdown_requested or retry_freq == None: break
+                time.sleep(retry_freq)
+            
+
     def debug_message(self, msg : str):
         if self.debug: print(msg)
 
     def shutdown_server(self):
-        if self.httpService: self.httpService.shutdown()
+        self.shutdown_requested = True
+        if self.server_running:
+            self.http_service.shutdown()
+    
+    def dereference_data(self):
+        self.obs_properties = None
+        self.obs_data = None
+        
+    def shutdown_server_async(self):
+        # TODO: Results in deadlock; No idea how fix.
+        watcher_thread : Thread = Thread(name="Server Shutdown Watcher", target=self.shutdown_server, daemon=False)
+        watcher_thread.start()
 
 
 
@@ -29,18 +78,19 @@ class SettingsRequestHandler(SimpleHTTPRequestHandler):
         GLOBAL_DATA.debug_message("Received request: " + self.path)
 
         query_param = self.path
-        
-        if query_param != '/?settings':
+
+        if query_param == '/?settings':
+            self.handle_settings_request()
+        else:
             self.send_error(404, "Invalid Request.", "Monitor config only supports settings requests.")
             GLOBAL_DATA.debug_message("Request rejected.")
-            return
 
-        # Send a response back to the client
+    def handle_settings_request(self):
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
-        settings_data = json.dumps(settings_as_dict(GLOBAL_DATA.obsData))
+        settings_data = json.dumps(settings_as_dict(GLOBAL_DATA.obs_data))
         GLOBAL_DATA.debug_message(settings_data)
         self.wfile.write(settings_data.encode())
         GLOBAL_DATA.debug_message("Request handled.")
@@ -52,33 +102,24 @@ class SettingsRequestHandler(SimpleHTTPRequestHandler):
 
 
 GLOBAL_DATA : GlobalData = GlobalData()
-
+RETRY_FREQUENCY : float = 0.5
 PORT = 6005
 CONFIG_FILE : str = os.path.relpath(__file__) + os.path.sep + "config.json"
 
 
 
 
-def serve_settings():
-    with TCPServer(("", PORT), SettingsRequestHandler) as service:
-        GLOBAL_DATA.httpService = service
-        GLOBAL_DATA.debug_message(f"Serving on port {PORT}")
-        service.serve_forever()
-
-
-
-
 def script_load(settings):
-    GLOBAL_DATA.obsData = settings
-    GLOBAL_DATA.debug_message('Starting settings server...')
-    server_thread = Thread(name="HTTP server", target=serve_settings)
-    server_thread.start()
+    script_unload()
+    GLOBAL_DATA.obs_data = settings
+    GLOBAL_DATA.start_server_asynch(RETRY_FREQUENCY)
 
 
 
 
 def script_unload():
-    GLOBAL_DATA.shutdown_server()
+    GLOBAL_DATA.shutdown_server_async()
+    GLOBAL_DATA.dereference_data()
 
 
 
@@ -89,7 +130,7 @@ def script_defaults(settings):
     obs.obs_data_set_default_bool(settings, "_use_pass", False)
     obs.obs_data_set_default_string(settings, "_address", "127.0.0.1")
     obs.obs_data_set_default_int(settings, "_port", 4455)
-    obs.obs_data_set_default_int(settings, "_color", int("32Cd32", 16))
+    obs.obs_data_set_default_int(settings, "_color", int("ff32Cd32", 16))
 
 
 
@@ -99,14 +140,13 @@ def script_description():
 
 
 
+
 def on_debug_toggled(props, prop, *args, **kwargs):
-    print(GLOBAL_DATA.debug)
     GLOBAL_DATA.debug = not GLOBAL_DATA.debug
-    print(GLOBAL_DATA.debug)
     debug_btn = obs.obs_properties_get(props, "_debug")
     obs.obs_property_set_description(debug_btn, f'Debug {"Enabled" if GLOBAL_DATA.debug else "Disabled"}')
-    print("Done")
     return True
+
 
 
 
@@ -129,7 +169,7 @@ def script_properties():
 
     #obs.obs_properties_add_button(props, "save_button", "Save Settings", save_config)
 
-    GLOBAL_DATA.obsProperties = props
+    GLOBAL_DATA.obs_properties = props
 
     return props
 
@@ -189,7 +229,7 @@ def load_config() :
 
 
 def add_filter_callback(props, prop, *args, **kwargs):
-    data = GLOBAL_DATA.obsData
+    data = GLOBAL_DATA.obs_data
     add_filter(
         data,
         filterName=obs.obs_data_get_string(data, "_filter"),
